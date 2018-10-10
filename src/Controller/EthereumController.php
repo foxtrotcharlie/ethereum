ecb4a851b732092a6248d002b48b4dc0abd7b3b4
Charles Tanton <charles@parkroad.co.za>
2018-10-10 16:21:14 +0200

    Move Perform Operation to it's own controller
    Controllers that extend EthereumController shouldn't inherit the performOperation method, so it has been moved to it's own controller, which implements ContainerInjectionInterface instead of ControllerBase because it doesn't require the additional utility methods, and I thought it's a nice way to keep it simple, and should make it easier to unit test.

--- src/Controller/EthereumController.php
+++ src/Controller/EthereumController.php
@@ -4,11 +4,7 @@ namespace Drupal\ethereum\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Ethereum\Ethereum;
-use Drupal\ethereum\EthereumServerInterface;
 use Drupal\Core\Render\Markup;
-use Symfony\Component\HttpFoundation\Request;
-use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\ReplaceCommand;
 
 /**
  * Controller routines for Ethereum routes.
@@ -58,37 +54,6 @@ class EthereumController extends ControllerBase {
       $html = strrev(implode('', explode(strrev('<hr />'), strrev($html), 2)));
       $this->messenger()->addMessage(Markup::create($html), 'warning');
     }
-  }
-
-
-  /**
-   * Calls an operation on an ethereum_server entity and reloads the listing page.
-   *
-   * @param \Drupal\ethereum\EthereumServerInterface $ethereum_server
-   *   The server being acted upon.
-   * @param string $op
-   *   The operation to perform, e.g., 'enable' or 'disable'.
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The current request.
-   *
-   * @return \Drupal\Core\Ajax\AjaxResponse|\Symfony\Component\HttpFoundation\RedirectResponse
-   *   Either returns a rebuilt listing page as an AJAX response, or redirects
-   *   back to the listing page.
-   */
-  public function performOperation(EthereumServerInterface $ethereum_server, $op, Request $request) {
-    // Perform the operation.
-    $ethereum_server->$op()->save();
-
-    // If the request is via AJAX, return the rendered list as JSON.
-    if ($request->request->get('js')) {
-      $list = $this->entityTypeManager()->getListBuilder('ethereum_server')->render();
-      $response = new AjaxResponse();
-      $response->addCommand(new ReplaceCommand('#ethereum-server-list', $list));
-      return $response;
-    }
-
-    // Otherwise, redirect back to the network settings page.
-    return $this->redirect('ethereum.settings');
   }
 
 }
